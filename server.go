a82c51c0cb4f9db77d4d7b8ebea3f39e559adedf
Yuvaraj Loganathan <uvaraj6@gmail.com>
2018-11-09 20:57:35 +0530

    Added go get example

--- server.go
+++ server.go
@@ -7,15 +7,19 @@ import (
 import (
 	"github.com/gorilla/mux"
 	"github.com/sirupsen/logrus"
+	"gopkg.in/alecthomas/kingpin.v2"
 )
 
 func main() {
+	port := kingpin.Flag("port", "Port Number to listen").Default("9999").Short('p').String()
+	kingpin.Parse()
 	r := mux.NewRouter()
 	r.HandleFunc("/", HomeHandler)
 	//r.HandleFunc("/products", ProductsHandler)
 	//r.HandleFunc("/articles", ArticlesHandler)
 	http.Handle("/", r)
-	http.ListenAndServe("0.0.0.0:9999", r)
+	logrus.Info(*port)
+	http.ListenAndServe("0.0.0.0:"+*port, r)
 }
 func HomeHandler(writer http.ResponseWriter, request *http.Request) {
 	io.WriteString(writer, "Hello World!")
